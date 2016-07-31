a2e4fce878a5cfacb476c23080172f811fb2aa83
vektory79 <vektory79@gmail.com>
2016-07-31 17:18:19 +0300

    i386 version of image with fixes for latest Ubuntu Xenial version

--- init/04_set_timezone.sh
+++ init/04_set_timezone.sh
@@ -3,6 +3,6 @@
 if [[ $(cat /etc/timezone) != "$TZ" ]] ; then
   echo "$TZ" > /etc/timezone
   dpkg-reconfigure -f noninteractive tzdata
-sed -i -e "s#;date.timezone.*#date.timezone = ${TZ}#g" /etc/php5/fpm/php.ini
-sed -i -e "s#;date.timezone.*#date.timezone = ${TZ}#g" /etc/php5/cli/php.ini
+sed -i -e "s#;date.timezone.*#date.timezone = ${TZ}#g" /etc/php/7.0/fpm/php.ini
+sed -i -e "s#;date.timezone.*#date.timezone = ${TZ}#g" /etc/php/7.0/cli/php.ini
 fi
